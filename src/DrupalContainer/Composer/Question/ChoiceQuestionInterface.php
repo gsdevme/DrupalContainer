a651e3a6bf03214f3035fd93884704ea84b772c3
Gavin Staniforth <gavinstaniforth@wems.co.uk>
2015-03-09 22:22:42 +0000

    Fixed the docblock

--- src/DrupalContainer/Composer/Question/ChoiceQuestionInterface.php
+++ src/DrupalContainer/Composer/Question/ChoiceQuestionInterface.php
@@ -4,5 +4,8 @@ namespace DrupalContainer\Composer\Question;
 
 interface ChoiceQuestionInterface extends QuestionInterface
 {
+    /**
+     * @return array|null
+     */
     public function getChoiceArray();
 }
